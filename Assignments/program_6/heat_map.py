14e086baddf7c479be522f44d9f5c1c7481264ff
cwcook0414 <c.w.cook@live.com>
2017-07-06 10:26:38 -0500

    program_6

--- Assignments/program_6/heat_map.py
+++ Assignments/program_6/heat_map.py
@@ -1,4 +1,24 @@
-'''[(u'Papumay', {'count': 1, 'coordinates': [-90.929862, 14.755234]})'''
+"""
+Program:
+--------
+    Program 6 - Heat Map
+
+Description:
+------------
+    Uses globalterrorism.geojson to plot cities that terrorism has occured in.
+    You MUST unzip globalterrorism.geojson.zip. It is too large to uploaded to 
+    github. If a city has has a lower number of terrist attacks, it is plotted 
+    with a blue dot with a small radius. The city with the most attacks will 
+    be plotted with a red dot with a large radius. All other cities will be
+    plotted according to the number of terrist attacks that have occured
+    there. The dots of the cities will have diffferent colors and sizes occord-
+    ing to the number of attacks. Finally, the window that contains the plotted
+    points is saved in the program_6 folder with the name "heat_map.png." The
+    saved image is overwritten EVERY time heat_map.py is run.
+    
+Name: Chris W Cook
+Date: 05 July 2017
+"""
 
 from mongo_features_helper import *
 from mapping_helper import *
@@ -34,8 +54,6 @@ mh = mongoHelper()
 #separate all cities into a dictionary that only holds: city name, coordinates_of city, terrorism_count
 all_cities_terror = {}
 
-result_list = []
-
 extremes = {}
 
 extremes['max_x'] = width
@@ -64,11 +82,8 @@ del all_cities_terror['Unknown']
 #sorts based on number of attacks
 sorted_cities = sorted(all_cities_terror.items(), key=lambda (k,v): v['count'])
 
-#first item in sorted_cities is the largest
-sorted_cities.reverse()
-
-minval = (sorted_cities[-1][1]['count'])
-maxval = (sorted_cities[0][1]['count'])
+minval = (sorted_cities[0][1]['count'])
+maxval = (sorted_cities[-1][1]['count'])
 
 #https://stackoverflow.com/questions/20792445/calculate-rgb-value-for-a-range-of-values-to-create-heat-map
 def rgb(minimum, maximum, value):
@@ -79,6 +94,8 @@ def rgb(minimum, maximum, value):
     g = 255 - b - r
     return r, g, b
 
+drawn = False
+
 running = True
 while running:
 
@@ -86,8 +103,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    for sc in sorted_cities:
-        pygame.draw.circle(screen, rgb(minval,maxval,sc[1]['count']), sc[1]['coordinates'], (sc[1]['count']//500),0)
-        pygame.display.flip()
+    if drawn == False:
+        for sc in sorted_cities:
+            pygame.draw.circle(screen, rgb(minval,maxval,sc[1]['count']), sc[1]['coordinates'], (sc[1]['count']//200),(sc[1]['count']//1000))
+            pygame.display.flip()
 
-    pygame.image.save(screen, DIRPATH+'/program_6.png')
+        pygame.image.save(screen, DIRPATH+'/heat_map.png')
+        drawn = True
